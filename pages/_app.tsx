c4778a494c8b03b0c947f70c75dfb2619e921eaa
Ali Gorji <ag.aligorji@gmail.com>
2023-04-06 15:39:51 +0330

    feat: import reset-css to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import 'reset-css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
